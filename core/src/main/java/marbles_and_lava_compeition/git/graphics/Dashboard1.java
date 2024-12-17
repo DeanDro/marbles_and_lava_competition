86d285421fd0671e1dc9c8dd0f519e9e9c4bec5e
Konstantinos Drosos <dean.drossos@gmail.com>
2024-12-17 17:12:04 -0500

    Continued with building the board

--- core/src/main/java/marbles_and_lava_compeition/git/graphics/Dashboard1.java
+++ core/src/main/java/marbles_and_lava_compeition/git/graphics/Dashboard1.java
@@ -36,6 +36,7 @@ import marbles_and_lava_compeition.git.utilities.CountriesPopulation;
 
 public class Dashboard1 extends ScreenAdapter {
 
+    public static final String BACKGROUND_IMAGES_KIDS_CHEERING_4_PNG = "background_images/kids_cheering4.png";
     public Main game;
     public String[] countriesGroups;
     public String[] winners;
@@ -51,6 +52,25 @@ public class Dashboard1 extends ScreenAdapter {
     private Sprite netSprite;
     private Texture goalieTexture;
     private Sprite goalieSprite;
+    private Texture grassTexture1;
+    private Sprite grassSprite1;
+    private Sprite grassSprite2;
+    private Sprite grassSprite3;
+    private Texture waterLavaTexture;
+    private Sprite waterLavaSprite;
+    private Sprite waterLavaSprite2;
+    private Sprite waterLavaSprite3;
+    private Sprite waterLavaSprite4;
+    private Sprite waterLavaSprite5;
+    private Sprite waterLavaSprite6;
+    private Texture pointsTexture1;
+    private Sprite pointsSprite1;
+    private Texture pointsTexture2;
+    private Sprite pointsSprite2;
+    private Texture pointsTexture3;
+    private Sprite pointsSprite3;
+    private Texture pointsTexture4;
+    private Sprite pointsSprite4;
 
 
     public Dashboard1(Main game, String[] groups, String gamer){
@@ -64,22 +84,81 @@ public class Dashboard1 extends ScreenAdapter {
     public void populatePlayerGraphics(){
 
         if (this.gamer.equals("Anastasia")){
-            this.peopleTexture = new Texture(Gdx.files.internal("background_images/kids_cheering4.png"));
+            this.peopleTexture = new Texture(Gdx.files.internal(BACKGROUND_IMAGES_KIDS_CHEERING_4_PNG));
+            this.waterLavaTexture = new Texture(Gdx.files.internal("background_images/water.png"));
+
             this.goalieTexture = new Texture(Gdx.files.internal("background_images/unicorn.png"));
+            this.goalieSprite = new Sprite(this.goalieTexture);
+
+            this.grassTexture1 = new Texture(Gdx.files.internal("background_images/grass.png"));
+            this.grassSprite1 = new Sprite(this.grassTexture1);
+            this.grassSprite1.setSize(1170, 50);
+            this.grassSprite1.setPosition(450, 0);
+            this.grassSprite1.draw(this.game.batch);
+
             this.netTexture = new Texture(Gdx.files.internal("background_images/nett.png"));
             this.netSprite = new Sprite(this.netTexture);
+            this.netSprite.setSize(50, 300);
+            this.netSprite.setPosition(1570, 50);
+            netSprite.draw(this.game.batch);
 
         } else {
             this.goalieTexture = new Texture(Gdx.files.internal("background_images/red_ball.png"));
             this.peopleTexture = new Texture(Gdx.files.internal("background_images/dinosaur.png"));
+            this.waterLavaTexture = new Texture(Gdx.files.internal("background_images/laval.png"));
+            this.waterLavaSprite = new Sprite(this.waterLavaTexture);
+            this.waterLavaSprite2 = new Sprite(this.waterLavaTexture);
+            this.waterLavaSprite3 = new Sprite(this.waterLavaTexture);
+            this.waterLavaSprite4 = new Sprite(this.waterLavaTexture);
+            this.waterLavaSprite5 = new Sprite(this.waterLavaTexture);
+            this.waterLavaSprite6 = new Sprite(this.waterLavaTexture);
+
+            this.waterLavaSprite.setSize(300, 50);
+            this.waterLavaSprite2.setSize(200, 50);
+            this.waterLavaSprite3.setSize(200, 50);
+            this.waterLavaSprite4.setSize(50, 150);
+            this.waterLavaSprite5.setSize(50, 150);
+            this.waterLavaSprite6.setSize(50, 150);
+
+            this.waterLavaSprite.setPosition(400, 0);
+            this.waterLavaSprite2.setPosition(900, 0);
+            this.waterLavaSprite3.setPosition(1300, 0);
+            this.waterLavaSprite4.setPosition(1570, 200);
+            this.waterLavaSprite5.setPosition(1570, 450);
+            this.waterLavaSprite6.setPosition(1570, 800);
+
+            this.waterLavaSprite.draw(this.game.batch);
+            this.waterLavaSprite2.draw(this.game.batch);
+            this.waterLavaSprite3.draw(this.game.batch);
+            this.waterLavaSprite4.draw(this.game.batch);
+            this.waterLavaSprite5.draw(this.game.batch);
+            this.waterLavaSprite6.draw(this.game.batch);
+
         }
 
-        this.goalieSprite = new Sprite(this.goalieTexture);
+
         this.peopleSprite = new Sprite(this.peopleTexture);
 
         this.peopleSprite.setPosition(600, 200);
         this.peopleSprite.setSize(400, 400);
         this.peopleSprite.draw(this.game.batch);
+
+    }
+
+    // Dispose textures handling each player differently
+    public void disposeTextures(){
+
+        if (this.gamer.equals("Anastasia")){
+            this.netTexture.dispose();
+            this.grassTexture1.dispose();
+
+        } else {
+
+            this.waterLavaTexture.dispose();
+
+        }
+
+        this.peopleTexture.dispose();
 
     }
 
@@ -118,7 +197,7 @@ public class Dashboard1 extends ScreenAdapter {
         this.stage.dispose();
 
         // Remove background graphics
-        this.peopleTexture.dispose();
+        this.disposeTextures();
 
     }
 }
