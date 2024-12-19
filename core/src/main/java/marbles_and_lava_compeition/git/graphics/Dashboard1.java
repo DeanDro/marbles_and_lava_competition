959130c778732bdcfb4bae6b56fd9061cf200acd
Konstantinos Drosos <dean.drossos@gmail.com>
2024-12-19 15:23:27 -0500

    Added goalie movement and entrance functionality

--- core/src/main/java/marbles_and_lava_compeition/git/graphics/Dashboard1.java
+++ core/src/main/java/marbles_and_lava_compeition/git/graphics/Dashboard1.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -28,6 +29,7 @@ public class Dashboard1 extends ScreenAdapter {
     public float timeLapsed;
     public float timeRemaining;
     public int previousTime;
+    public int ballsCounter;
 
     // Helper classes
     DashboardUtilities utilities = new DashboardUtilities();
@@ -37,8 +39,6 @@ public class Dashboard1 extends ScreenAdapter {
     private Sprite peopleSprite;
     private Texture netTexture;
     private Sprite netSprite;
-    private Texture goalieTexture;
-    private Sprite goalieSprite;
     private Texture grassTexture1;
     private Sprite grassSprite1;
     private Sprite grassSprite2;
@@ -56,8 +56,26 @@ public class Dashboard1 extends ScreenAdapter {
     private Sprite pointsSprite3;
     private Texture trambolineTexture;
     private Sprite trambolineSprite;
-    private Label min;
-    private Label sec;
+
+    // Moving elements
+    private Texture pipeTexture;
+    private Sprite pipeSprite;
+    private Texture ballTexture1;
+    private Sprite ballSprite1;
+    private Texture ballTexture2;
+    private Sprite ballSprite2;
+    private Texture ballTexture3;
+    private Sprite ballSprite3;
+    private Texture ballTexture4;
+    private Sprite ballSprite4;
+    private Texture goalieTexture;
+    private Sprite goalieSprite;
+
+    // Movement components elements
+    private int goalieSpeed = 1;
+    private int goalieY;
+    private int pipeSpeed;
+    private int pipeRotation;
 
 
     public Dashboard1(Main game, String[] groups, String gamer){
@@ -65,6 +83,7 @@ public class Dashboard1 extends ScreenAdapter {
         this.countriesGroups = groups;
         this.winners = new String[(groups.length)/2];  // Only half of the marbles will be making it to the next round
         this.gamer = gamer;
+        this.ballsCounter = 0;
     }
 
     // Creates the dashboard according to the player
@@ -75,20 +94,11 @@ public class Dashboard1 extends ScreenAdapter {
         this.trambolineTexture = new Texture(Gdx.files.internal("background_images/tranpolin.png"));
         this.trambolineSprite = new Sprite(this.trambolineTexture);
 
-        int remMin = (int) (timeRemaining/60);
-        int remSec = (int) (timeRemaining % 60);
-
-
-
         if (this.gamer.equals("Anastasia")){
             this.peopleTexture = new Texture(Gdx.files.internal(BACKGROUND_IMAGES_KIDS_CHEERING_4_PNG));
             this.waterLavaTexture = new Texture(Gdx.files.internal("background_images/water.png"));
             this.waterLavaSprite = new Sprite(this.waterLavaTexture);
             this.utilities.setSpritePosSizeDraw(this.game, this.waterLavaSprite, 250, 0, 200, 50);
-
-
-            this.goalieTexture = new Texture(Gdx.files.internal("background_images/unicorn.png"));
-            this.goalieSprite = new Sprite(this.goalieTexture);
 
             this.grassSprite1 = new Sprite(this.grassTexture1);
             this.utilities.setSpritePosSizeDraw(this.game, this.grassSprite1, 450, 0, 1170, 50);
@@ -100,7 +110,7 @@ public class Dashboard1 extends ScreenAdapter {
             this.utilities.setSpritePosSizeDraw(this.game, this.trambolineSprite, 0, 0, 250, 100);
 
         } else {
-            this.goalieTexture = new Texture(Gdx.files.internal("background_images/red_ball.png"));
+
             this.peopleTexture = new Texture(Gdx.files.internal("background_images/dinosaur.png"));
             this.waterLavaTexture = new Texture(Gdx.files.internal("background_images/laval.png"));
 
@@ -158,6 +168,51 @@ public class Dashboard1 extends ScreenAdapter {
         this.peopleTexture.dispose();
         this.grassTexture1.dispose();
         this.trambolineTexture.dispose();
+        this.goalieTexture.dispose();
+        this.pipeTexture.dispose();
+
+    }
+
+    // manage moving elements
+    public void movingElements(){
+
+        // Check user
+        if (this.gamer.equals("Anastasia")){
+            this.goalieTexture = new Texture(Gdx.files.internal("background_images/unicorn.png"));
+            this.pipeTexture = new Texture(Gdx.files.internal("background_images/pipe.png"));
+        } else {
+            this.goalieTexture = new Texture(Gdx.files.internal("background_images/red_ball.png"));
+            this.pipeTexture = new Texture(Gdx.files.internal("background_images/blue_pipe.png"));
+        }
+        this.goalieSprite = new Sprite(this.goalieTexture);
+        this.pipeSprite = new Sprite(this.pipeTexture);
+
+        if (this.pipeRotation > 70 || this.pipeRotation < -70){
+            this.pipeSpeed *= -1;
+        }
+        this.pipeRotation += this.pipeSpeed;
+        this.pipeSprite.setRotation(this.pipeRotation);
+        this.utilities.setSpritePosSizeDraw(this.game, this.pipeSprite, 300, 830, 50, 250);
+
+
+        // Different goalie move
+        if (this.gamer.equals("Anastasia")){
+            this.utilities.setSpritePosSizeDraw(this.game, this.goalieSprite, 1400, this.goalieY, 150, 100);
+
+            if (this.goalieY > 300 || this.goalieY < 50){
+                this.goalieSpeed *= -1;
+            }
+
+
+        } else {
+            this.utilities.setSpritePosSizeDraw(this.game, this.goalieSprite, 1400, this.goalieY, 160, 160);
+            if (this.goalieY > 800 || this.goalieY < 50){
+                this.goalieSpeed *= -1;
+            }
+
+        }
+
+        this.goalieY += this.goalieSpeed;
 
     }
 
@@ -168,9 +223,18 @@ public class Dashboard1 extends ScreenAdapter {
         Gdx.input.setInputProcessor(stage);
 
         // Two minutes per game
-        this.timeRemaining = 2 * 1000;
+        this.timeRemaining = 2 * 60;
         this.timeLapsed = 0;
         this.previousTime = 0;
+
+        // Moving elements initial positions
+        this.goalieY = 50;
+        this.pipeRotation = 0;
+        this.pipeSpeed = 1;
+
+        if (this.gamer.equals("William")){
+            this.goalieSpeed = 15;
+        }
 
     }
 
@@ -187,7 +251,7 @@ public class Dashboard1 extends ScreenAdapter {
 
         if (timeRemaining > 0){
             timeLapsed += Gdx.graphics.getDeltaTime();
-            timeRemaining--;
+            timeRemaining -= Gdx.graphics.getDeltaTime();
         }
 
         this.game.batch.begin();
@@ -197,7 +261,11 @@ public class Dashboard1 extends ScreenAdapter {
 
         int remMin = (int) (timeRemaining/60);
         int remSec = (int) (timeRemaining % 60);
+        font.getData().setScale(2f);
         font.draw(game.batch, String.format("%02d:%02d", remMin, remSec), 1000, 800);
+
+        // Moving elements
+        this.movingElements();
 
         this.stage.draw();
         this.game.batch.end();
